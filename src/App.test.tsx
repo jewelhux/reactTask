ced6bec47c4d2f6f511db8d68d9458c4b31bece4
Jik789 <stix.jik@gmail.com>
2023-03-13 13:47:06 +0300

    fix: test name

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,9 @@
 import { render } from '@testing-library/react';
-import React from 'react';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { act } from 'react-dom/test-utils';
 import { CardComponent } from './components/pages/common/CardComponent';
 
-// This is the section where we mock `fetch`
 const unmockedFetch = global.fetch;
 
 beforeAll(() => {
@@ -20,8 +18,8 @@ afterAll(() => {
 });
 
 describe('App', () => {
-  it('should work as expected', async () => {
-    await act(() => {
+  it('all page', () => {
+    act(() => {
       render(
         <BrowserRouter>
           <App />
@@ -30,26 +28,17 @@ describe('App', () => {
     });
   });
 
-  it('testing', async () => {
-    await act(() => {
+  it('one card', () => {
+    act(() => {
       const testProduct = {
         id: 1,
         title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
         price: 109.95,
         description:
           'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
-        category: "men's clothing",
         image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-        rating: {
-          rate: 3.9,
-          count: 120,
-        },
       };
       render(<CardComponent product={testProduct}></CardComponent>);
     });
   });
-
-  // it('adds 2 numbers', () => {
-  //   expect(1 + 1).toBe(2);
-  // });
 });
