571167cd7f90dbd37530abe52d3ef12c7d092fa4
Jik789 <stix.jik@gmail.com>
2023-03-23 16:12:34 +0300

    feat: test utils and validates

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,19 @@
+import React, { RefObject } from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { act } from 'react-dom/test-utils';
 import { CardComponent } from './components/pages/common/CardComponent';
+import {
+  validateCategory,
+  validateCondition,
+  validateDate,
+  validateImage,
+  validateName,
+} from './components/utils/validates';
+import { NotFoundPage } from './components/pages/NotFoundPage';
+import { AboutPage } from './components/pages/AboutPage';
+import { targetRadio } from './components/utils/utils';
 
 const unmockedFetch = global.fetch;
 
@@ -28,6 +39,18 @@ describe('App', () => {
     });
   });
 
+  it('not found page', async () => {
+    await act(() => {
+      render(<NotFoundPage />);
+    });
+  });
+
+  it('about page', async () => {
+    await act(() => {
+      render(<AboutPage />);
+    });
+  });
+
   it('one card', async () => {
     await act(() => {
       const testProduct = {
@@ -40,6 +63,78 @@ describe('App', () => {
         category: 'car',
       };
       render(<CardComponent product={testProduct}></CardComponent>);
+    });
+  });
+
+  it('validate name', async () => {
+    await act(() => {
+      expect(validateName('Not')).toBe(false);
+      expect(validateName('Super')).toBe(true);
+    });
+  });
+
+  it('validate image', async () => {
+    await act(() => {
+      expect(validateImage('image')).toBe(true);
+      expect(validateImage('')).toBe(false);
+    });
+  });
+
+  it('validate condition', async () => {
+    await act(() => {
+      expect(validateCondition('new')).toBe(true);
+      expect(validateCategory('')).toBe(false);
+    });
+  });
+
+  it('validate category', async () => {
+    await act(() => {
+      expect(validateCondition('new')).toBe(true);
+      expect(validateCategory('')).toBe(false);
+    });
+  });
+
+  it('validate date', async () => {
+    await act(() => {
+      expect(validateDate('one')).toBe(false);
+    });
+  });
+
+  it('target radio', async () => {
+    await act(() => {
+      const refNew = { current: { checked: false } };
+      const refOld = { current: { checked: false } };
+
+      const result = targetRadio(
+        refNew as RefObject<HTMLInputElement>,
+        refOld as RefObject<HTMLInputElement>
+      );
+
+      expect(result).toBe('');
+    });
+
+    await act(() => {
+      const refNew = { current: { checked: true, value: '1' } };
+      const refOld = { current: { checked: false, value: '2' } };
+
+      const result = targetRadio(
+        refNew as RefObject<HTMLInputElement>,
+        refOld as RefObject<HTMLInputElement>
+      );
+
+      expect(result).toBe('1');
+    });
+
+    await act(() => {
+      const refNew = { current: { checked: false, value: '1' } };
+      const refOld = { current: { checked: true, value: '2' } };
+
+      const result = targetRadio(
+        refNew as RefObject<HTMLInputElement>,
+        refOld as RefObject<HTMLInputElement>
+      );
+
+      expect(result).toBe('2');
     });
   });
 });
