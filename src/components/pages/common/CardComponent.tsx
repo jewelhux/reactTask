ce340c84f4431047f10a0e4d2830c74e4c3630eb
Jik789 <stix.jik@gmail.com>
2023-03-27 10:40:33 +0300

    refactor: start task3, refactor card component

--- src/components/pages/common/CardComponent.tsx
+++ src/components/pages/common/CardComponent.tsx
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ProductProps } from '../../utils/type';
 
-export class CardComponent extends Component<ProductProps> {
-  constructor(props: ProductProps) {
-    super(props);
-  }
-
-  render() {
-    const item = this.props.product;
-    return (
-      <div className="item-card">
-        <h2>{item.title}</h2>
-        <div className="item-description">
-          <p>Category: {item.category}</p>
-          <p>Condition: {item.condition}</p>
-          <p>Date: {item.date}</p>
-        </div>
-        <div className="img-container">
-          <img src={item.image} alt="card-img" />
-        </div>
+export function CardComponent(props: ProductProps) {
+  const item = props.product;
+  return (
+    <div className="item-card">
+      <h2>{item.title}</h2>
+      <div className="item-description">
+        <p>Category: {item.category}</p>
+        <p>Condition: {item.condition}</p>
+        <p>Date: {item.date}</p>
       </div>
-    );
-  }
+      <div className="img-container">
+        <img src={item.image} alt="card-img" />
+      </div>
+    </div>
+  );
 }
