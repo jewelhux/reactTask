4a84475e372bd67afa8b34239b8beb289088ced5
Jik789 <stix.jik@gmail.com>
2023-03-30 10:28:05 +0300

    refactor: delete comment

--- src/components/utils/type.ts
+++ src/components/utils/type.ts
@@ -19,12 +19,4 @@ type InputsForm = {
   inputRules: boolean;
 };
 
-//     this.inputTitleRef = React.createRef();
-//     this.inputImageRef = React.createRef();
-//     this.inputDateRef = React.createRef();
-//     this.inputСonditionRefNew = React.createRef();
-//     this.inputСonditionRefOld = React.createRef();
-//     this.inputRulesRef = React.createRef();
-//     this.inputCategoryRef = React.createRef();
-
 export type { HeadProps, ProductProps, FormAddProps, InputsForm };
